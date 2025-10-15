a620bc9a97049c8a9ab31edecc723f7b72b5fe21
tnovakED <tiagonovaksbs@gmail.com>
2025-10-14 21:35:33 -0300

    Alteracao da pipeline para leitura do arquivo xlsx e movimentar esse arquivo para outra pasta de arquivos processados

--- src/extract.py
+++ src/extract.py
@@ -1,18 +1,37 @@
+# This Python script is performing the following tasks:
 import pandas as pd
-import pathlib as Path
+import shutil
+from pathlib import Path
 
-drive_path = Path(r"C:\Users\Tiago\Google Drive\SigaoCharmeETL\data")
-
+drive_path = Path(r"G:\Meu Drive\SigaoCharmeETL\data")
 landing_path = drive_path / "landing_files"
 bronze_path = drive_path / "bronze"
+file_processed_folder = drive_path / "files_processed"
 
-
-csv_files = landing_path / "vendas_itens.csv"
-df = pd.read_csv(csv_files, sep=',', encoding='utf-8')
-
-parquet_file = bronze_path / "vendas_item.parquet"
-
+# Criar as pastas se nao existirem
 bronze_path.mkdir(parents=True, exist_ok=True)
-df.to_parquet(parquet_file, index=False)
+file_processed_folder.mkdir(parents=True, exist_ok=True)
 
-print(f"Arquivo salvo em: {parquet_file}")
+excel_files = landing_path / "venda_itens.xlsx"
+
+if not excel_files.exists():
+    raise FileNotFoundError(f"O Arquivo nao foi encontrado: {excel_files}")
+
+df = pd.read_excel(excel_files)
+
+#Verificando a quantidade de linhas que tem a palavra CONSUMIDOR, previamente verificada, para excluí-las
+#num_oc = df['Título'].astype(str).str.upper().str.contains('CONSUMIDOR').sum()
+#print(f'numerom de ocorrencias: {num_oc}')
+
+#Eliminando essas linhas
+df = df[~df['Título'].astype(str).str.startswith('1 - CONSUMIDOR')]
+
+#salvando em csv para a pasta bronze para depois tratar tipos de dados
+csv_file = bronze_path/"venda_itens.csv"
+df.to_csv(csv_file, index=False)
+
+arquivo_processado = Path(file_processed_folder)
+
+for arquivo in landing_path.glob("*.xlsx"):
+    destino = file_processed_folder / arquivo.name
+    shutil.move(arquivo, destino)
